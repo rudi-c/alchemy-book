d63ff746470df36f51035c81330bbab071ccd909
Rudi Chen <rudichen@gmail.com>
2017-07-27 19:44:14 -0400

    Write tests with permutations of events

--- test/js/test_editor.ts
+++ test/js/test_editor.ts
@@ -1,5 +1,7 @@
 import test from "ava"
 
+import * as Combinatorics from "js-combinatorics"
+
 import * as Char from "../../web/static/js/char"
 import * as Crdt from "../../web/static/js/crdt"
 import Editor from "../../web/static/js/editor"
@@ -72,6 +74,10 @@ class TestEditorSocket extends EditorSocket {
             this.letOneThrough();
         }
     }
+
+    public duplicateAllMessages() {
+        this.messageQueue = this.messageQueue.concat(this.messageQueue);
+    }
 }
 
 class TestEditor extends Editor {
@@ -87,16 +93,28 @@ class TestEditor extends Editor {
         doc.replaceRange(insertion, doc.getCursor(), doc.getCursor(), "+input");
     }
 
+    public delete(count: number = 1): void {
+        for (let i = 0; i < count; i++) {
+            (this.codemirror as any).execCommand("delCharAfter");
+        }
+    }
+
+    public backspace(count: number = 1): void {
+        for (let i = 0; i < count; i++) {
+            (this.codemirror as any).execCommand("delCharBefore");
+        }
+    }
+
     public moveCursor(update: (pos: CodeMirror.Position) => CodeMirror.Position): void {
         const doc = this.codemirror.getDoc();
         doc.setCursor(update(doc.getCursor()));
     }
 
-    public moveCursorLeft(amount: number): void {
+    public moveCursorLeft(amount: number = 1): void {
         this.moveCursor(({ line, ch }) => ({ line, ch: ch - amount }));
     }
 
-    public moveCursorRight(amount: number): void {
+    public moveCursorRight(amount: number = 1): void {
         this.moveCursor(({ line, ch }) => ({ line, ch: ch + amount }));
     }
 
@@ -106,6 +124,10 @@ class TestEditor extends Editor {
 
     public letAllThrough(): void {
         (this.editorSocket as TestEditorSocket).letAllThrough();
+    }
+
+    public duplicateAllMessages(): void {
+        (this.editorSocket as TestEditorSocket).duplicateAllMessages();
     }
 
     public init(val: Char.Serial[], site: number): void {
@@ -131,6 +153,25 @@ function createEditors(n: number): TestEditor[] {
     return editors;
 }
 
+// To make sure that the system behaves as expected regardless of the order that
+// different events happen on the client, we want to test all permutations on the
+// order of those events.
+//
+// The function `runPermutations` permutes the action of `editorCount` editors
+// by giving the test function a permutation runner that takes an array of thunks,
+// and runs those thunks in some order.
+type RunPermutation = (thunks: (() => void)[]) => void
+function runPermutations(editorCount: number, test: (_: RunPermutation) => void): void {
+    Combinatorics.permutation([...Array(editorCount).keys()])
+                 .forEach(permutation => {
+        test(thunks => {
+            permutation.forEach(i => {
+                thunks[i]();
+            })
+        });
+    })
+}
+
 test("simple insertion at various places", t => {
     const [e1, e2] = createEditors(2);
     e1.type("b");
@@ -144,7 +185,157 @@ test("simple insertion at various places", t => {
     e1.type("a");
     e1.moveCursorRight(1);
     e1.type("c");
+
+    e1.letAllThrough();
     e2.letAllThrough();
     t.is(e1.getText(), "abc\nabc");
     t.is(e2.getText(), "abc\nabc");
+});
+
+test("insertions and deletes are idempotent", t => {
+    runPermutations(2, runPermutation => {
+        const [e1, e2] = createEditors(2);
+        e1.type("ac");
+
+        e1.letAllThrough();
+        e2.letAllThrough();
+
+        runPermutation([
+            () => { 
+                e1.backspace();
+                e1.type("b");
+            },
+            () => {
+                e2.moveCursorRight(2);
+                e2.backspace();
+            }
+        ])
+
+        e1.duplicateAllMessages();
+        e2.duplicateAllMessages();
+
+        e1.letAllThrough();
+        e2.letAllThrough();
+
+        t.is(e1.getText(), "ab");
+        t.is(e2.getText(), "ab");
+    });
+});
+
+test("conflicting simple insertions at the same place", t => {
+    runPermutations(3, runPermutation => {
+        const [e1, e2, e3] = createEditors(3);
+        e1.type("ab");
+
+        e1.letAllThrough();
+        e2.letAllThrough();
+        e3.letAllThrough();
+
+        e1.moveCursor(() => ({ line: 0, ch: 1 }));
+        e2.moveCursor(() => ({ line: 0, ch: 1 }));
+        e3.moveCursor(() => ({ line: 0, ch: 1 }));
+
+        runPermutation([
+            () => e1.type("z"),
+            () => e2.type("y"),
+            () => e3.type("x")
+        ]);
+
+        e1.letAllThrough();
+        e2.letAllThrough();
+        e3.letAllThrough();
+
+        t.is(e1.getText(), "azyxb");
+        t.is(e2.getText(), "azyxb");
+        t.is(e3.getText(), "azyxb");
+    });
+});
+
+test("conflicting insertion between deleted markers", t => {
+    runPermutations(3, runPermutation => {
+        const [e1, e2, e3] = createEditors(3);
+        e1.type("ab");
+
+        e1.letAllThrough();
+        e2.letAllThrough();
+        e3.letAllThrough();
+
+        e1.moveCursor(() => ({ line: 0, ch: 2 }));
+        e2.moveCursor(() => ({ line: 0, ch: 1 }));
+        e3.moveCursor(() => ({ line: 0, ch: 1 }));
+
+        runPermutation([
+            () => e1.backspace(2),
+            () => e2.type("x"),
+            () => e3.type("y")
+        ]);
+
+        e1.letAllThrough();
+        e2.letAllThrough();
+        e3.letAllThrough();
+
+        t.is(e1.getText(), "xy");
+        t.is(e2.getText(), "xy");
+        t.is(e3.getText(), "xy");
+    });
+});
+
+test("interleaved insertions affecting offsets on the same line", t => {
+    runPermutations(3, runPermutation => {
+        const [e1, e2, e3] = createEditors(3);
+
+        e1.type("abc");
+
+        e1.letAllThrough();
+        e2.letAllThrough();
+        e3.letAllThrough();
+
+        e1.moveCursor(() => ({ line: 0, ch: 1 }));
+        e2.moveCursor(() => ({ line: 0, ch: 2 }));
+        e3.moveCursor(() => ({ line: 0, ch: 3 }));
+
+        runPermutation([
+            () => e1.type("1"),
+            () => e2.type("2"),
+            () => e3.type("3")
+        ]);
+
+        e1.letAllThrough();
+        e2.letAllThrough();
+        e3.letAllThrough();
+
+        t.is(e1.getText(), "a1b2c3");
+        t.is(e2.getText(), "a1b2c3");
+        t.is(e3.getText(), "a1b2c3");
+    });
+});
+
+test("interleaved insertions pushing cursors on new lines", t => {
+    runPermutations(3, runPermutation => {
+        const [e1, e2, e3] = createEditors(3);
+
+        e1.type("abc");
+
+        e1.letAllThrough();
+        e2.letAllThrough();
+        e3.letAllThrough();
+
+        e1.moveCursor(() => ({ line: 0, ch: 1 }));
+        e2.moveCursor(() => ({ line: 0, ch: 2 }));
+        e3.moveCursor(() => ({ line: 0, ch: 3 }));
+
+        runPermutation([
+            () => e1.type("1\n"),
+            () => e2.type("2\n"),
+            () => e3.type("3\n")
+        ]);
+
+        e1.letAllThrough();
+        e2.letAllThrough();
+        e3.letAllThrough();
+
+        t.is(e1.getText(), "a1\nb2\nc3\n");
+        t.is(e2.getText(), "a1\nb2\nc3\n");
+        t.is(e3.getText(), "a1\nb2\nc3\n");
+    });
 });
