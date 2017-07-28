7b73414b3ef7cd886890ec5edf564872984ae9f2
Rudi Chen <rudichen@gmail.com>
2017-07-28 10:41:52 -0400

    Thicker editor cursor

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,8 +2,6 @@ import "phoenix_html"
 
 import renderCollaborator from "./collaborator"
 
-require("codemirror/lib/codemirror.css");
-
 const collaborator = document.getElementById("collaborator");
 if (collaborator) {
     renderCollaborator(collaborator);
